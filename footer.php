3345d860522be1adeb05750bd6e1d0f999f9a5ad
rishigole <51227917+rishigole42@users.noreply.github.com>
2019-08-02 08:55:03 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+	echo '<footer class="footer navbar-inverse navbar-fixed-bottom">
+      <div class="container">
+        <p class="text-muted" align="center">Copyright &copy; Dues, 2019.</p>
+      </div>
+    </footer>';
+?>
